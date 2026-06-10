cf7748e4c19348a4da16bb210a48c49624046230
Craig Walls <habuma@gmail.com>
2026-06-10 06:54:28 -0600

    Add Essential RAG code

---
+++ essential-rag/src/main/java/com/example/essentialrag/EssentialRagApplication.java
@@ -0,0 +1,35 @@
+package com.example.essentialrag;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class EssentialRagApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EssentialRagApplication.class, args);
+  }
+
+  @Bean
+  ApplicationRunner go(ChatClient chatClient) {
+    return args -> {
+      System.out.println("How can I help?\n");
+      try (Scanner scanner = new Scanner(System.in)) {
+        while (true) {
+          System.out.print("> ");
+          System.out.println("\n - " +
+              chatClient.prompt(scanner.nextLine())
+                  .advisors(spec -> spec.param(ChatMemory.CONVERSATION_ID, "DEMO"))
+                  .call().content());
+        }
+      }
+    };
+  }
+
+}
