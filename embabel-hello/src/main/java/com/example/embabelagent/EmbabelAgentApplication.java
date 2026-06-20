bfcff9a4f1a5384c178f28c7530e78b3c53758f5
Craig Walls <habuma@gmail.com>
2026-06-20 11:11:58 -0600

    Add Embabel hello world example

---
+++ embabel-hello/src/main/java/com/example/embabelagent/EmbabelAgentApplication.java
@@ -0,0 +1,13 @@
+package com.example.embabelagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbabelAgentApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EmbabelAgentApplication.class, args);
+  }
+
+}
---
+++ embabel-hello/src/main/java/com/example/embabelagent/HelloAgent.java
@@ -0,0 +1,33 @@
+package com.example.embabelagent;
+
+import com.embabel.agent.api.annotation.AchievesGoal;
+import com.embabel.agent.api.annotation.Action;
+import com.embabel.agent.api.annotation.Agent;
+import com.embabel.agent.api.annotation.Export;
+import com.embabel.agent.api.common.Ai;
+import com.embabel.agent.domain.io.UserInput;
+
+@Agent(name = "helloAgent",
+    description = "Says hello to someone.")
+public class HelloAgent {
+
+  @Action(description = "Derive Person from input")
+  public Person derivePerson(UserInput userInput, Ai ai) {
+    var prompt = String.format("Derive the person from the input: %s", userInput.getContent());
+    return ai
+        .withDefaultLlm()
+        .createObject(prompt, Person.class);
+  }
+
+  @Action(description = "Say hello to someone")
+  @AchievesGoal(
+      description = "Someone has been greeted with a friendly hello",
+      export=@Export(
+          name = "sayHello",
+          remote = true,
+          startingInputTypes = Person.class))
+  public Greeting sayHello(Person person) {
+    return new Greeting(String.format("Hello, %s!", person.name()));
+  }
+
+}
