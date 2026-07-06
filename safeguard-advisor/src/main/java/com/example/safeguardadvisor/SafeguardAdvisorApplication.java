a5c54accc57b2d84c73e87c22352ea686f0af833
Craig Walls <habuma@gmail.com>
2026-07-06 09:00:01 -0600

    Add SafeGuardAdvisor example code

---
+++ safeguard-advisor/src/main/java/com/example/safeguardadvisor/SafeguardAdvisorApplication.java
@@ -0,0 +1,40 @@
+package com.example.safeguardadvisor;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class SafeguardAdvisorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SafeguardAdvisorApplication.class, args);
+  }
+
+  @Bean
+  ApplicationRunner go(ChatClient chatClient) {
+    return args -> {
+      System.out.println("How can I help?\n");
+
+      try (Scanner scanner = new Scanner(System.in)) {
+        while (true) {
+          System.out.print("> ");
+          if (!scanner.hasNextLine()) break; // to avoid infinite loops in tests
+          var input = scanner.nextLine();
+          if (input.isBlank()) continue; // allows user to hit return without error
+          System.out.println("\n - " +
+              chatClient.prompt(input)
+                  .advisors(spec -> spec.param(ChatMemory.CONVERSATION_ID, "DEMO"))
+                  .call()
+                  .content());
+        }
+      }
+    };
+  }
+
+}
