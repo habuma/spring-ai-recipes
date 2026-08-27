b2d1c5d18496deb9d910b153a82e1f3fb285ccdc
Craig Walls <habuma@gmail.com>
2026-08-27 08:12:05 -0600

    Add ingester code to HyDE RAG example

---
+++ hyde-rag/spring-boot-docs-ingestor/src/main/java/com/example/springbootdocsingestor/SpringBootDocsIngestorApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdocsingestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDocsIngestorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootDocsIngestorApplication.class, args);
+  }
+
+}
